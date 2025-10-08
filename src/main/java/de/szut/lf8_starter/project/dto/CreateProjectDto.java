1adefd555afc57d9fbab18268ee9464b1387bc87
momari <momari@cargosoft.de>
2025-10-08 10:30:29 +0200

    PMS-18: Projekt anlegen, REST-Endpoint POST /projects erstellen.

--- src/main/java/de/szut/lf8_starter/project/dto/CreateProjectDto.java
+++ src/main/java/de/szut/lf8_starter/project/dto/CreateProjectDto.java
@@ -1,7 +1,30 @@
 package de.szut.lf8_starter.project.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class CreateProjectDto {
+
+    @NotNull
+    private String designation;
+
+    @NotNull
+    private Long responsibleEmployeeId;
+
+    @NotNull
+    private Long customerId;
+
+    private String customerContactPerson;
+
+    @NotNull
+    private String comment;
+
+    private LocalDate startDate;
+
+    private LocalDate plannedEndDate;
+
+    private LocalDate endDate;
 }
--- src/main/java/de/szut/lf8_starter/project/dto/GetProjectDto.java
+++ src/main/java/de/szut/lf8_starter/project/dto/GetProjectDto.java
@@ -2,6 +2,18 @@ package de.szut.lf8_starter.project.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class GetProjectDto {
+
+    private Long id;
+    private String designation;
+    private Long responsibleEmployeeId;
+    private Long customerId;
+    private String customerContactPerson;
+    private String comment;
+    private LocalDate startDate;
+    private LocalDate plannedEndDate;
+    private LocalDate endDate;
 }
