c8088d89ac95c42e404063cec3300de16e323a5d
momari <momari@cargosoft.de>
2025-10-27 13:51:43 +0100

    PMS-42 + PMS-44: Projekt löschen, REST-Endpoint DELETE /projects/{id} erstellen und Erfolgsantwort Status 200.

--- src/main/java/de/szut/lf8_starter/project/ProjectController.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectController.java
@@ -1,4 +1,25 @@
 package de.szut.lf8_starter.project;
 
+
+import de.szut.lf8_starter.mapping.MappingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/projects")
 public class ProjectController {
+
+    private final ProjectService service;
+    private final MappingService mappingService;
+
+    public ProjectController(ProjectService service, MappingService mappingService) {
+        this.service = service;
+        this.mappingService = mappingService;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteProject(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
