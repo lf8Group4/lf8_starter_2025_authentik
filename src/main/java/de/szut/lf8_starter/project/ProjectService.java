fed19daf660aad2278e5e6454067c60ef958e09c
momari <momari@cargosoft.de>
2025-11-02 17:47:09 +0100

    PMS-20: Fehlerfälle Mitarbeiter/Kunde → 404 umgesetzt

--- src/main/java/de/szut/lf8_starter/project/ProjectService.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectService.java
@@ -1,5 +1,7 @@
 package de.szut.lf8_starter.project;
 
+import de.szut.lf8_starter.customer.CustomerServiceClient;
+import de.szut.lf8_starter.employee.EmployeeServiceClient;
 import de.szut.lf8_starter.exceptionHandling.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +11,31 @@ import java.util.List;
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
+    private final EmployeeServiceClient employeeServiceClient;
+    private final CustomerServiceClient customerServiceClient;
 
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, EmployeeServiceClient employeeServiceClient, CustomerServiceClient customerServiceClient) {
         this.projectRepository = projectRepository;
+        this.employeeServiceClient = employeeServiceClient;
+        this.customerServiceClient = customerServiceClient;
+
     }
+
+    /**
+     * Erstellt ein neues Projekt. Führt Validierungen gegen externe Services durch.
+     */
+    public ProjectEntity create(ProjectEntity projectEntity) {
+        if (!employeeServiceClient.employeeExists(projectEntity.getResponsibleEmployeeId())) {
+            throw new ResourceNotFoundException("Responsible Employee with ID " + projectEntity.getResponsibleEmployeeId() + " does not exist.");
+        }
+
+        if (!customerServiceClient.customerExists(projectEntity.getCustomerId())) {
+            throw new ResourceNotFoundException("Customer with ID " + projectEntity.getCustomerId() + " does not exist.");
+        }
+
+        return projectRepository.save(projectEntity);
+    }
+
 
     public ProjectEntity getById(Long id) {
         return projectRepository.findById(id)
