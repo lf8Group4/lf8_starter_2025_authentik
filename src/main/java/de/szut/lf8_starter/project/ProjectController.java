4c42db6e8283a6ae909d56b4dfd555ce7f678601
momari <momari@cargosoft.de>
2025-11-05 11:08:41 +0100

    PMS-23 + PMS-24 + PMS-25 + PMS-35 + PMS-36 + PMS-37 + PMS-38 + PMS-53 + PMS-54 + PMS-55 + PMS-56. End Punkte implementiert.

--- src/main/java/de/szut/lf8_starter/project/ProjectController.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectController.java
@@ -4,6 +4,11 @@ package de.szut.lf8_starter.project;
 import de.szut.lf8_starter.mapping.MappingService;
 import de.szut.lf8_starter.project.dto.CreateProjectDto;
 import de.szut.lf8_starter.project.dto.GetProjectDto;
+import de.szut.lf8_starter.project.dto.ProjectEmployeesGetDto;
+import de.szut.lf8_starter.project_employee.ProjectEmployeeService;
+import de.szut.lf8_starter.project_employee.dto.AddProjectEmployeeDto;
+import de.szut.lf8_starter.project_employee.dto.AddedProjectEmployeeGetDto;
+import de.szut.lf8_starter.project_employee.dto.DeleteConfirmationDto;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +23,12 @@ public class ProjectController implements ProjectControllerOpenAPI {
 
     private final ProjectService service;
     private final MappingService mappingService;
+    private final ProjectEmployeeService projectEmployeeService;
 
-    public ProjectController(ProjectService service, MappingService mappingService) {
+    public ProjectController(ProjectService service, MappingService mappingService, ProjectEmployeeService projectEmployeeService) {
         this.service = service;
         this.mappingService = mappingService;
+        this.projectEmployeeService = projectEmployeeService;
     }
 
     @Override
@@ -31,6 +38,16 @@ public class ProjectController implements ProjectControllerOpenAPI {
         newProject = service.create(newProject);
         GetProjectDto responseDto = mappingService.mapProjectEntityToGetProjectDto(newProject);
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+
+    @Override
+    @PutMapping("/{id}")
+    public ResponseEntity<GetProjectDto> updateProject(@PathVariable Long id, @RequestBody @Valid CreateProjectDto updateProjectDto) {
+        ProjectEntity projectToUpdate = mappingService.mapCreateProjectDtoToProject(updateProjectDto);
+        ProjectEntity updatedProject = service.update(id, projectToUpdate);
+        GetProjectDto responseDto = mappingService.mapProjectEntityToGetProjectDto(updatedProject);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @Override
@@ -58,4 +75,33 @@ public class ProjectController implements ProjectControllerOpenAPI {
     public void deleteProject(@PathVariable Long id) {
         service.delete(id);
     }
+
+    // POST /projects/{id}/employees (Mitarbeiter hinzufügen)
+    @Override
+    @PostMapping("/{id}/employees")
+    public ResponseEntity<AddedProjectEmployeeGetDto> addEmployeeToProject(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid AddProjectEmployeeDto dto) {
+
+        AddedProjectEmployeeGetDto response = projectEmployeeService.addEmployeeToProject(id, dto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    //DELETE /projects/{projectId}/employees/{employeeId} (Mitarbeiter entfernen)
+    @DeleteMapping("/{projectId}/employees/{employeeId}")
+    public ResponseEntity<DeleteConfirmationDto> removeEmployeeFromProject(
+            @PathVariable Long projectId,
+            @PathVariable Long employeeId) {
+
+        DeleteConfirmationDto response = projectEmployeeService.removeEmployeeFromProject(projectId, employeeId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // GET /projects/{id}/employees (Zugewiesene Mitarbeiter abrufen)
+    @GetMapping("/{id}/employees")
+    public ResponseEntity<ProjectEmployeesGetDto> getProjectEmployees(@PathVariable Long id) {
+        ProjectEmployeesGetDto dto = service.getProjectEmployees(id);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
 }
